7cb2c6226027ccc8a193ec607cd1758856e6ec91
Chen Yue <yuechen049@gmail.com>
2024-05-18 14:46:38 -0400

    added getCol, getRow, getGrid for Sudoku class and added enum class for sudoku values

--- composeApp/src/commonMain/kotlin/gameLogic/Sudoku.kt
+++ composeApp/src/commonMain/kotlin/gameLogic/Sudoku.kt
@@ -1,17 +1,46 @@
 package gameLogic
 
-data class Cell (var value: Int)
+enum class SudokuValue(val num: Int?) {
+    EMPTY(null),
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4)
+}
+data class Cell (var value: SudokuValue)
 
 class Sudoku {
     private var sudokuGrid = MutableList<Cell>(16) {
-        Cell(0)
+        Cell(SudokuValue.EMPTY)
     }
 
-    // get the n-th row of a sudoku grid
+    /**
+     * @return the grid of the sudoku game
+     */
+    fun getGrid(): List<Cell>{
+        return sudokuGrid
+    }
+
+    /**
+     * @param n the number n for the n-th row of the sudoku grid
+     * @return a list of the cells of the n-th row
+     */
     fun getRow(n: Int): List<Cell>{
         val li = mutableListOf<Cell>()
         for (i in 0..3){
             li.add(sudokuGrid[(n*4)+i])
+        }
+        return li
+    }
+
+    /**
+     * @param n the number n for the n-th column of the sudoku grid
+     * @return a list of the cells of the n-th column
+     */
+    fun getCol(n: Int): List<Cell>{
+        val li = mutableListOf<Cell>()
+        for (i in 0..3){
+            li.add(sudokuGrid[(i*4)+n])
         }
         return li
     }
