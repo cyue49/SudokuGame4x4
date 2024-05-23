33943c5a54b42b23103bfb4039fd2c95b5fee62b
Chen Yue <yuechen049@gmail.com>
2024-05-22 22:30:02 -0400

    added backtrack function that checks whether a 4x4 sudoku puzzle has a unique solution, and added function that generates a random 4x4 sudoku puzzle that has a unique solution

--- composeApp/src/commonMain/kotlin/gameLogic/Sudoku.kt
+++ composeApp/src/commonMain/kotlin/gameLogic/Sudoku.kt
@@ -20,6 +20,11 @@ data class Cell (
 )
 
 /**
+ * Class that serves as a counter
+ */
+class Counter (var count: Int = 0)
+
+/**
  * Data class representing a Sudoku game
  * @property sudokuGrid a List of Cell representing the sudoku grid
  */
@@ -169,38 +174,57 @@ data class Sudoku (
         return Pair(row, col)
     }
 
+    /**
+     * Get a new random 4x4 sudoku puzzle that has a unique solution
+     * @return A list of cells representing the sudoku grid
+     */
     private fun getNewGameGrid(): List<Cell> {
+        // Get a random valid and completed sudoku grid using backtracking algorithm on an empty grid
         val randomValuesList = mutableListOf(1,2,3,4)
         val li: MutableList<Cell> = MutableList<Cell>(16) { Cell(SudokuValue.EMPTY) }
         backtrack(li, 0, randomValuesList.shuffled())
 
-        /*
-        * TODO:
-        * ==> PART 1: Generate a random valid completed board
-        * ==> PART 2 randomly remove cells until a sudoku grid with solvable & unique solution
-        */
+        // Remove values from cells in a random order until the sudoku grid has a unique solvable solution
+        val cellsRemoveOrder: MutableList<Int> = MutableList<Int>(16) { it } // list containing the order in which values will be removed from cells
+        cellsRemoveOrder.shuffle()
+
+        while (cellsRemoveOrder.isNotEmpty()){
+            val nextCell = cellsRemoveOrder.removeAt(0) // get next cell position to try to remove
+            val cellValue = li[nextCell].value // remember the cell's value in case this cell's value should not be removed
+            li[nextCell].value = SudokuValue.EMPTY // remove value at this cell
+
+            // check if grid still has unique solution
+            val liCopy: MutableList<Cell> = MutableList<Cell>(16) { Cell(SudokuValue.EMPTY) }
+            for (i in li.indices){
+                liCopy[i] = li[i].copy()
+                if (liCopy[i].value != SudokuValue.EMPTY) liCopy[i].isInputField = false
+            }
+
+            // if no, put back value & change cell type to not input cell
+            if (backtrack(liCopy, 0, Counter(0)) != 1){
+                li[nextCell].value = cellValue
+                li[nextCell].isInputField = false
+            }
+        }
 
         return li
     }
 
     /**
-     * Backtracking algorithm to solve a 4x4 sudoku puzzle
+     * Backtracking algorithm that solves a 4x4 sudoku puzzle
      * @param cellList the list of cell representing the sudoku grid
      * @param nextEmptyCell the index of the next cell to be solved in the list
      * @param randomValuesList a shuffled list of values 1 to 4 following which the backtracking algorithm will attempt to fill in the values
-     * @return true if the sudoku grid is filled up successfully with a valid and complete grid, false otherwise
+     * @return true if the sudoku grid is solved successfully, false otherwise
      */
     private fun backtrack(cellList: MutableList<Cell>, nextEmptyCell: Int, randomValuesList: List<Int>): Boolean{
         if (nextEmptyCell == cellList.size) { // if filled in all cells, return
-            return  true
+            return true
         }
 
         // if this cell is not an input cell go next
         if (!cellList[nextEmptyCell].isInputField) {
-            if (backtrack(cellList, nextEmptyCell+1, randomValuesList.shuffled())){
-                return true
-            }
-            return false
+            return backtrack(cellList, nextEmptyCell+1, randomValuesList.shuffled())
         }
 
         for (i in randomValuesList){ // for each values 1 to 4
@@ -215,6 +239,33 @@ data class Sudoku (
             }
         }
         return false
+    }
+
+    /**
+     * Backtracking algorithm that checks if a 4x4 sudoku puzzle has a unique solution or not
+     * @param cellList the list of cell representing the sudoku grid
+     * @param nextEmptyCell the index of the next cell to be solved in the list
+     * @param counter counter that keeps track of the current number of solutions
+     * @return 0 if no solution, 1 if unique solution, 2 if more than one solution
+     */
+    private fun backtrack(cellList: MutableList<Cell>, nextEmptyCell: Int, counter: Counter): Int{
+        if (nextEmptyCell == cellList.size) {
+            return counter.count + 1 // +1 to counter when found a solution
+        }
+
+        if (!cellList[nextEmptyCell].isInputField) {
+            return backtrack(cellList, nextEmptyCell+1, counter)
+        }
+
+        for (i in 1..4){
+            if (counter.count > 1) break // if more than 1 solution, sudoku grid does not have a unique solution, break
+            cellList[nextEmptyCell].value = SudokuValue.entries[i]
+            if (validateGrid(cellList,true)) {
+                counter.count = backtrack(cellList, nextEmptyCell+1, counter)
+            }
+            cellList[nextEmptyCell].value = SudokuValue.EMPTY
+        }
+        return counter.count
     }
 
     /**
