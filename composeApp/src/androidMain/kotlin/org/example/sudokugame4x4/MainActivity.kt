3b9b941b555fdf2347898dbbc6f4ce9a6cd0347e
yandeng63 <154352421+yandeng63@users.noreply.github.com>
2024-05-25 21:49:15 -0400

    splashscreen

--- composeApp/src/androidMain/kotlin/org/example/sudokugame4x4/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/sudokugame4x4/MainActivity.kt
@@ -18,18 +18,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
             setOnExitAnimationListener { screen ->
-                val zoomX = ObjectAnimator.ofFloat(screen.iconView, View.SCALE_X, 0.4f, 0.0f)
+                /*val zoomX = ObjectAnimator.ofFloat(screen.iconView, View.SCALE_X, 0.4f, 0.0f)
                 zoomX.interpolator = OvershootInterpolator()
-                zoomX.duration = 500L
+                zoomX.duration = 1500L
                 zoomX.doOnEnd { screen.remove() }
 
                 val zoomY = ObjectAnimator.ofFloat(screen.iconView, View.SCALE_Y, 0.4f, 0.0f)
                 zoomY.interpolator = OvershootInterpolator()
-                zoomY.duration = 500L
+                zoomY.duration = 1500L
                 zoomY.doOnEnd { screen.remove() }
 
                 zoomX.start()
-                zoomY.start()
+                zoomY.start()*/
+                val alpha = ObjectAnimator.ofFloat(screen.iconView, View.ALPHA, 1f, 0f)
+                alpha.duration = 2000L
+                alpha.doOnEnd { screen.remove() }
+
+                alpha.start()
             }
         }
         setContent {
--- composeApp/src/commonMain/kotlin/CompletionScreen.kt
+++ composeApp/src/commonMain/kotlin/CompletionScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 
 data class CompletionScreen(
+    val username: String,
     val time: String,
     val onNewGame: () -> Unit,
     val onHome: () -> Unit
@@ -29,7 +30,7 @@ data class CompletionScreen(
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Excellent!",
+                text = "Excellent, $username!",
                 fontSize = 32.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFF2878FF)
