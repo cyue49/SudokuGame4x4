96dc5d0e59f225d6a43d3751619f4c9a900cfbef
Chen Yue <yuechen049@gmail.com>
2024-05-26 11:39:03 -0400

    modified window size for desktop app

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.window.rememberWindowState
 import cafe.adriel.voyager.navigator.Navigator
 
 fun main() = application {
-    val windowState = rememberWindowState(width = 800.dp, height = 800.dp)
+    val windowState = rememberWindowState(width = 800.dp, height = 700.dp)
     Window(
         onCloseRequest = ::exitApplication,
         title = "SudokuGame4x4",
