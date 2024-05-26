5cec4ba77b92851375fdcdc645b966bde2f0d164
Chen Yue <yuechen049@gmail.com>
2024-05-26 11:35:57 -0400

    added window size to desktop app

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,19 +1,19 @@
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-//import gameLogic.SudokuViewModel
-//import gameLogic.TestApp
+import androidx.compose.ui.window.rememberWindowState
 import cafe.adriel.voyager.navigator.Navigator
 
 fun main() = application {
+    val windowState = rememberWindowState(width = 800.dp, height = 800.dp)
     Window(
         onCloseRequest = ::exitApplication,
         title = "SudokuGame4x4",
+        state = windowState
     ) {
-//        TestApp(viewModel = SudokuViewModel())
-
         MaterialTheme {
             Surface {
                 Navigator(screen = HomeScreen)
